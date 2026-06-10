9ef40f02bef5f2dfb651dc5ebea94fb1990bbe38
Devansh Rusiya <120799940+devansh-rusiya01@users.noreply.github.com>
2026-06-10 17:14:24 +0530

    d
    d

--- models.py
+++ models.py
@@ -1,6 +1,14 @@
 from pydantic import BaseModel
+from typing import Optional
 
-class UserIn(BaseModel):
-    username: str
+class UserSignup(BaseModel):
+    first_name: str
+    last_name: str
+    email: str
+    password: str
+    phone_number: Optional[str] = None
+    role: str = "user"
+
+class UserLogin(BaseModel):
     email: str
     password: str
